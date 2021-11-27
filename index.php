1f0bbd146c98da567d2054166a6ca6410a17ec4e
Roman <romanrus98@mail.ru>
2021-11-27 06:49:42 +0500

    Сверстал часть слайдера

--- index.php
+++ index.php
@@ -7,13 +7,39 @@
     <link rel="stylesheet" href="assets\font\stylesheet.css">
     <link rel="stylesheet" href="assets\css\default.css">
     <link rel="stylesheet" href="assets\css\header.css">
-    <link rel="stylesheet" href="assets\css\home.css">
+    <link rel="stylesheet" href="assets\css\main.css">
     <title>GreenShop</title>
 </head>
 <body>
     <div class="container">
         <?php include "blocks/header.html" ?>
-        <main></main>
+        <main class="main">
+            <div class="slider">
+                <div class="slider__wrapper">
+                    <div class="slider__items">
+                        <div class="slider__item">
+                            <div class="slider__left">
+                                <h3 class="slider__subtitle">Welcome to GreenShop</h3>
+                                <h1 class="slider__title slider__title_mt10">Let’s Make a Better <span>Planet</span></h1>
+                                <p class="slider__text">We are an online plant shop offering a wide range of cheap and trendy plants. 
+                                    Use our plants to create an unique Urban Jungle. Order your favorite plants!</p>
+                                <button class="btn btn_mt44">SHOP NOW</button>     
+                            </div>
+                            <div class="slider__right">
+                                <img class="slider__img" src="assets\img\slider-img\1.png" alt="flower">
+                            </div>
+                        </div>
+                        <div class="slider__item"></div>
+                        <div class="slider__item"></div>
+                    </div>
+                </div>
+                <ul class="controls">
+                    <li class="controls__item"><button class="controls__btn"></button></li>
+                    <li class="controls__item"><button class="controls__btn active"></button></li>
+                    <li class="controls__item"><button class="controls__btn"></button></li>
+	            </ul>
+            </div>
+        </main>
         <?php include "blocks/footer.html" ?>
     </div>
 </body>
